1df97f666509c7b9dec2832015e4805e64fc8634
ahmedmujtaba1 <ahmed.mujtaba11737@hhs.edu.pk>
2023-12-14 17:44:07 +0500

    Adding Cookies

--- main.py
+++ main.py
@@ -29,7 +29,11 @@ link_wait_time = 4.2
 print(f"[+] Requesting the url search of Linkedin. Waiting for {link_wait_time} second/s")
 driver.get(url)
 time.sleep(link_wait_time)
-user_input = input("Are you done? [y/n]: ")
-cookies = driver.get_cookies()
-with open('cookies.pkl', 'wb') as f:
-    pickle.dump(cookies, f)
+# user_input = input("Are you done? [y/n]: ")
+# cookies = driver.get_cookies()
+# with open('cookies.pkl', 'wb') as f:
+#     pickle.dump(cookies, f)
+with open('cookies.pkl', 'rb') as f:
+    cookies = pickle.load(f)
+for cookie in cookies:
+    driver.add_cookie(cookie)
